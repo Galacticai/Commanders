1ae58be9d4cf38c944734ad7fdcc3a58feba2e05
xeroling <nhkomaiha@hotmail.com>
2022-05-14 19:09:05 +0300

    update to a more consistent naming scheme and disable warnings for intentional stuff

--- Assets/Objects/Entity/Unit/Unit.cs
+++ Assets/Objects/Entity/Unit/Unit.cs
@@ -6,21 +6,26 @@ namespace Assets.Objects.Entity.Unit {
     /// or any game entity that acts similarly </summary>
     internal class Unit : Entity {
 
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
         internal record Default {
             internal const float range = 5;
-            internal const float vision_to_range_factor = 0.2f;
-            internal const float power_criticalMultiplier = 1.5f;
+            internal const float vision_to_range_Factor = 0.2f;
+            internal const float power_critical_Multiplier = 1.5f;
         }
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
 
         internal protected float power { get; protected set; }
         internal protected float power_critical { get; protected set; }
         internal protected float range { get; protected set; }
 
+#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
         internal protected virtual float update_vision() {
             base.update_vision();
             this.update_range();
             return this.vision;
         }
+#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
+
         internal protected virtual float update_range() {
             this.range
                 = Default.range
