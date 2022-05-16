85edb5cca7f84e6d483cfa6874421707eb395fed
xeroling <nhkomaiha@hotmail.com>
2022-05-16 19:21:56 +0300

    move to `_Types_` namespace && mark abstract && make `update_vision` `override` inheritance && remove ugly-looking `#pragma` lines

--- Assets/Scripts/Objects/Entity/_Types_/Unit/Unit.cs
+++ Assets/Scripts/Objects/Entity/_Types_/Unit/Unit.cs
@@ -1,28 +1,24 @@
 using UnityEngine;
 
-namespace Assets.Scripts.Objects.Entity.Unit {
+namespace Assets.Scripts.Objects.Entity._Types_.Unit {
 
-    internal class Unit : Entity {
+    internal abstract class Unit : Entity {
 
-#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
         internal record Default {
             internal const float range = 5;
             internal const float vision_to_range_Factor = 0.2f;
             internal const float powerCritical_Factor = 1.5f;
         }
-#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
 
         internal protected float power { get; protected set; }
         internal protected float power_critical { get; protected set; }
         internal protected float range { get; protected set; }
 
-#pragma warning disable CS0114 // Member hides inherited member; missing override keyword
-        internal protected virtual float update_vision() {
+        internal protected override float update_vision() {
             base.update_vision();
             this.update_range();
             return this.vision;
         }
-#pragma warning restore CS0114 // Member hides inherited member; missing override keyword
 
         internal protected virtual float update_range() {
             this.range
