68f7d4ba680bcd9e8eecb9eb7ea8ededc66d4b34
xeroling <nhkomaiha@hotmail.com>
2022-05-16 19:17:15 +0300

    init Math.Common && init Common.forcedInRange

---
+++ Assets/Scripts/Lib/Math/Common.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Lib.Math {
+    public class Common {
+        public static double forcedInRange(double x, double min, double max) {
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
+        }
+    }
+}
