a36d879756c7edb4d7251285016780cf7385f052
xeroling <nhkomaiha@hotmail.com>
2022-05-16 19:16:06 +0300

    init Lib.Math.Space.Point

---
+++ Assets/Scripts/Lib/Math/Space/Point.cs
@@ -0,0 +1,25 @@
+﻿using sMath = System.Math;
+
+namespace Assets.Scripts.Lib.Math.Space {
+    public class Point {
+        private static Point _ORIGIN = new(0, 0, 0);
+        public static Point ORIGIN => Point._ORIGIN;
+
+
+        public double distance(Point point)
+            => sMath.Sqrt(
+                sMath.Pow(this.x - point.x, 2)
+                + sMath.Pow(this.y - point.y, 2)
+                + sMath.Pow(this.z - point.z, 2));
+
+        public double distanceToOrigin
+            => this.distance(Point.ORIGIN);
+
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public Point(double x, double y, double z) {
+            this.x = x; this.y = y; this.z = z;
+        }
+    }
+}
