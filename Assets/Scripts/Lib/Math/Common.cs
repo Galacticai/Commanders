daafcca69cd1f6ba9961f0c1fa9b45c37df09929
xeroling <nhkomaiha@hotmail.com>
2022-05-19 16:42:08 +0300

    move forcedInRange to Lib.Math.Range

--- Assets/Scripts/Lib/Math/Common.cs
+++ Assets/Scripts/Lib/Math/Common.cs
@@ -1,9 +1,3 @@
 ﻿namespace Assets.Scripts.Lib.Math {
-    public class Common {
-        public static double forcedInRange(double x, double min, double max) {
-            if (x < min) return min;
-            if (x > max) return max;
-            return x;
-        }
-    }
+    public class Common { }
 }
