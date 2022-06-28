548475f9dd48f85f3198211b2b3411cb9a63f206
xeroling <nhkomaiha@hotmail.com>
2022-06-29 00:22:49 +0300

    add RectanglePoints (Get {Point}s from {Rectangle})

---
+++ Assets/Scripts/Lib/Math/Space2D/RectanglePoints.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Scripts.Lib.Math.Space2D {
+    public class RectanglePoints {
+        public Point o { get; }
+        public Point ox { get; }
+        public Point oy { get; }
+        public Point oxy { get; }
+        public RectanglePoints(Rectangle rectangle) {
+            //? (x, y)
+            this.o = new(rectangle.x, rectangle.y);
+            //? (x+w, y)
+            this.ox = new(rectangle.x + rectangle.xLength, rectangle.y);
+            //? (x, y+h)
+            this.oy = new(rectangle.x, rectangle.y + rectangle.yLength);
+            //? (x+w, y+h)
+            this.oxy = new(rectangle.x + rectangle.xLength, rectangle.y + rectangle.yLength);
+        }
+    }
+}
