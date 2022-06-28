e49e82d1947d50a6c018b512cb33533f49d44893
xeroling <nhkomaiha@hotmail.com>
2022-06-29 00:23:00 +0300

    add Square (2D mathematical shape)

---
+++ Assets/Scripts/Lib/Math/Space2D/Square.cs
@@ -0,0 +1,21 @@
+﻿namespace Assets.Scripts.Lib.Math.Space2D {
+    internal class Square : Rectangle {
+        private new readonly double xLength, yLength;
+
+        public double length {
+            get => base.xLength;
+            set {
+                base.xLength = value;
+                base.yLength = value;
+            }
+        }
+
+        public Point center
+            => new(base.x + (this.length / 2), base.y + (this.length / 2));
+
+        public Square(double x, double y, double length)
+                    : base(x, y, length, length) { }
+        public Square(Point point, double length)
+                    : base(point, length, length) { }
+    }
+}
