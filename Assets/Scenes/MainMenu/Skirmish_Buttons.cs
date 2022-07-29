8095aa19bbc3b1dae1a3fdf1e5b27838b9ad6961
xeroling <nhkomaiha@hotmail.com>
2022-07-29 14:46:20 +0300

    init Skirmish_Buttons
    Buttons actions for Skirmish screen

---
+++ Assets/Scenes/MainMenu/Skirmish_Buttons.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Assets.Scenes.MainMenu {
+    public class Skirmish_Buttons : MonoBehaviour {
+
+        public void Demand_Button_Click() {
+
+        }
+
+
+    }
+}
