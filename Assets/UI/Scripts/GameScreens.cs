28766c99ba34225cee8169133809b68d3401c269
xeroling <nhkomaiha@hotmail.com>
2022-08-15 16:45:03 +0300

    init {GameScreens}
    methods related to `<UIDocument>`, `rootVisualElement` and `.Screen` `VisualElement` inside of them

---
+++ Assets/UI/Scripts/GameScreens.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.UI.Scripts {
+    internal static class GameScreens {
+        internal enum ScreenName {
+            Main_Screen,
+            Skirmish_Screen,
+            Background_Screen
+        }
+        internal static GameObject getScreen(ScreenName screenName)
+            => GameObject.Find(screenName.ToString());
+
+    }
+}
