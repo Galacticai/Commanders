dab9262559dd320df963c899670aaa3101da3f96
xeroling <nhkomaiha@hotmail.com>
2022-08-15 16:39:32 +0300

    init {FollowPointer}
    moves the `GameObject` `<Transform>` component along the chosen axis (2D motion in 3D)

---
+++ Assets/Scripts/Lib/Unity/FollowPointer.cs
@@ -0,0 +1,24 @@
+using Assets.Scripts.Lib.Math;
+using UnityEngine;
+
+public class FollowPointer : MonoBehaviour {
+    [SerializeField] private Common.Space.Plane _targetPlane;
+    [SerializeField] private Canvas _alignToCanvas;
+    private Transform _transform;
+
+    private void Start() {
+        this._transform = this.GetComponent<Transform>();
+    }
+    private void Update() {
+        if (this._transform == null) return;
+        Vector3 newPos = this._targetPlane switch {
+            Common.Space.Plane.xy => new(Input.mousePosition.x, Input.mousePosition.y, this._transform.position.z),
+            Common.Space.Plane.yz => new(this._transform.position.x, Input.mousePosition.x, Input.mousePosition.y),
+            Common.Space.Plane.xz => new(Input.mousePosition.x, this._transform.position.y, Input.mousePosition.y),
+        };
+        if (this._alignToCanvas != null) {
+
+        }
+        this._transform.position = newPos;
+    }
+}
