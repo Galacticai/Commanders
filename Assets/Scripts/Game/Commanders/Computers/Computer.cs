6cf512743133c2f7a5022b98ec494a3097902356
xeroling <nhkomaiha@hotmail.com>
2022-08-15 16:33:38 +0300

    improve security by verifying the integrity of the instance being created
    Basically, just make sure MASTER is not duplicated

--- Assets/Scripts/Game/Commanders/Computers/Computer.cs
+++ Assets/Scripts/Game/Commanders/Computers/Computer.cs
@@ -1,8 +1,11 @@
-﻿namespace Assets.Scripts.Game.Commanders.Computers {
+﻿using System;
+using System.Security;
+
+namespace Assets.Scripts.Game.Commanders.Computers {
     internal class Computer : Commander {
-        /// <summary> The master of all <see cref="Computer"/>s and <see cref="Player"/>s <br/>
+        /// <summary> The master of all <see cref="Player"/>s and <see cref="Computer"/>s <br/>
         /// Controls everything </summary>
-        internal readonly Computer MAIN = new();
+        internal static readonly Computer MASTER = new();
 
         /// <summary> The fighting technique of this <see cref="Commander"/> </summary>
         internal enum Strategy {
@@ -18,6 +21,15 @@
 
         internal Computer(int computerIndex, Strategy strategy, Provenance provenance, int alliance)
                     : base($"Computer_{computerIndex}_{strategy}_{provenance}_{alliance}", provenance, alliance) {
+            //!?
+            //!? Commander.Computer.MASTER is the only real MASTER commander (Already created above)
+            //!?
+            //?     > Clone of the real MASTER Commander is being created
+            if (base.isMASTER)
+                throw new VerificationException("Fake MASTER Commander detected! Aborting.");
+            //?     > New MASTER Commander is being created (not an accurate clone)
+            if (base.name == "MASTER" || base.provenance == Provenance.MASTER || base.alliance == 0)
+                throw new UnauthorizedAccessException("MASTER Commander is created automatically for better security. (Commander.Computer.MASTER)");
             this.strategy = strategy;
         }
     }
