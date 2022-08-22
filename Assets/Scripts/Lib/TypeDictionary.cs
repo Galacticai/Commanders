0fbb4d57e19873772d34daabb8a6a54c809632a9
xeroling <nhkomaiha@hotmail.com>
2022-08-22 21:03:51 +0300

    update `StatDictionary` to `TypeDictionary`
    Dictionary of `(TargetType)` with the `System.Type` of `(TargetType)` as the key

---
+++ Assets/Scripts/Lib/TypeDictionary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Lib {
+    /// <summary> Dictionary of <typeparamref name="TargetType"/> with the <see cref="System.Type"/> of <typeparamref name="TargetType"/> as the key </summary>
+    internal class TypeDictionary<TargetType> : Dictionary<System.Type, TargetType> {
+
+        //!? ==== HIDDEN ==========
+#pragma warning disable CS0108 // Member hides inherited member; missing new keyword
+        internal void Add(System.Type type, TargetType value)
+            => base.Add(type, value);
+        internal bool TryAdd(System.Type type, TargetType value)
+            => base.TryAdd(type, value);
+        internal void Add(TargetType value)
+            => this.Add(value.GetType(), value);
+        internal bool TryAdd(TargetType value)
+            => this.TryAdd(value.GetType(), value);
+#pragma warning restore CS0108 // Member hides inherited member; missing new keyword
+        //!? ======================
+
+        internal TypeDictionary(params TargetType[] values) {
+            foreach (var value in values) this.Add(value);
+        }
+        internal TypeDictionary() { }
+    }
+}
