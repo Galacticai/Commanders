51176ffdb9724b2b6a0d69384c173868a0aedc2f
xeroling <nhkomaiha@hotmail.com>
2022-10-02 02:36:53 +0300

    update to use the new code style

--- Assets/Animations/Fade.cs
+++ Assets/Animations/Fade.cs
@@ -1,11 +1,10 @@
-using Assets.Scripts.Lib.Math;
+using Assets.Scripts.Lib.Math.Numerics;
 using UnityEngine;
-using static Assets.Scripts.Lib.Math.Function;
 
 namespace Assets.Animations {
 
     public class Fade : MonoBehaviour {
-        [SerializeField] private FunctionName function;
+        [SerializeField] private Function.FunctionName function;
         private float _currentAnimationStep;
         [SerializeField] private float _fadeIn_Multiplier = 3, _fadeOut_Multiplier = 4;
         [SerializeField] private CanvasGroup _canvasGroup;
@@ -13,37 +12,37 @@ namespace Assets.Animations {
 
         private bool _shown = false;
         public void showOnce() {
-            if (this._shown) return;
-            this._shown = true;
-            this.show();
+            if (_shown) return;
+            _shown = true;
+            show();
         }
         public void resetShown()
-            => this._shown = false;
+            => _shown = false;
 
         public void show() {
-            this._canvasGroup.interactable = true;
-            this._fadeIn = true;
+            _canvasGroup.interactable = true;
+            _fadeIn = true;
         }
         public void hide() {
-            this._canvasGroup.interactable = false;
-            this._fadeOut = true;
+            _canvasGroup.interactable = false;
+            _fadeOut = true;
         }
 
         private void Start() {
-            this._currentAnimationStep = this._canvasGroup.alpha;
+            _currentAnimationStep = _canvasGroup.alpha;
         }
         private void Update() {
-            if (this._fadeIn && this._currentAnimationStep < 1) {
-                this._currentAnimationStep += Time.deltaTime * this._fadeIn_Multiplier;
-                this._canvasGroup.alpha = (float)Function.fx(this.function, this._currentAnimationStep, 0, 1);
-                if (this._currentAnimationStep >= 1)
-                    this._fadeIn = false;
+            if (_fadeIn && _currentAnimationStep < 1) {
+                _currentAnimationStep += Time.deltaTime * _fadeIn_Multiplier;
+                _canvasGroup.alpha = (float)Function.Fx(function, _currentAnimationStep, 0, 1);
+                if (_currentAnimationStep >= 1)
+                    _fadeIn = false;
             }
-            if (this._fadeOut && this._currentAnimationStep >= 0) {
-                this._currentAnimationStep -= Time.deltaTime * this._fadeIn_Multiplier;
-                this._canvasGroup.alpha = (float)Function.fx(this.function, this._currentAnimationStep, 0, 1);
-                if (this._currentAnimationStep <= 0)
-                    this._fadeOut = false;
+            if (_fadeOut && _currentAnimationStep >= 0) {
+                _currentAnimationStep -= Time.deltaTime * _fadeIn_Multiplier;
+                _canvasGroup.alpha = (float)Function.Fx(function, _currentAnimationStep, 0, 1);
+                if (_currentAnimationStep <= 0)
+                    _fadeOut = false;
             }
         }
     }
