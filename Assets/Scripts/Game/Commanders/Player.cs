56151051833e55b9ecfb903112718b365fc2cc7d
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:07:57 +0300

    feat(placeholder): Player type of a `Commander`

--- Assets/Scripts/Game/Commanders/Player.cs
+++ Assets/Scripts/Game/Commanders/Player.cs
@@ -2,13 +2,12 @@
 
 namespace Assets.Scripts.Game.Commanders {
     internal class Player : Commander {
-
+        internal bool IsOnNetwork { get; }
         internal Player(string name, Provenance provenance, int alliance, string cameraID, bool replace = false)
                     : base($"Player_n{name}_p{provenance}_a{alliance}", provenance, alliance, cameraID, replace) {
-
             if (cameraID.Length <= "Camera_Player_".Length)
                 throw new AccessViolationException($"The specified camera is invalid for the type \"{typeof(Player)}\"");
-
         }
+
     }
 }
