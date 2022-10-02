017f469a86861725f86d88536da2767a727a6aca
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:20:33 +0300

    migrate presets to their corresponding classes

--- Assets/Scripts/Game/Presets/unit.cs
+++
@@ -1,9 +0,0 @@
-namespace Assets.Scripts.Game.Presets {
-
-    /// <summary> Shortcuts for <see cref="Unit"/> </summary>
-    internal struct unit {
-        //internal Unit sampleUnit(GameObject gameObject)
-        //    => new(gameObject, Unit.Type.Human, Unit.Territory.Land);
-    }
-
-}
