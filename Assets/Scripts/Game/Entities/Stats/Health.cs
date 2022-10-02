01ac68db68385dac996b4e810f8cbce7b5929519
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:34:54 +0300

    update `Stat.Health`
    + feat: add defence/piercing
    + update to the new code style...

--- Assets/Scripts/Game/Entities/Stats/Health.cs
+++ Assets/Scripts/Game/Entities/Stats/Health.cs
@@ -1,81 +1,124 @@
+using Assets.Scripts.Lib.Math.Numerics;
 using System;
+using System.Threading;
 
 namespace Assets.Scripts.Game.Entities.Stats {
+    internal class Health : Stat {
+        #region Shortcuts
 
-    internal sealed class Health : Stat {
+        /// <summary> Maximum value for <see cref="Amount"/> </summary>
         internal float Total
             => Convert.ToSingle(Amount.Range.Max);
-            => this.health < this.healthTotal;
-        /// <returns> <see cref="health"/> == <see cref="healthTotal"/> </returns>
-        internal bool isHealed
-            => this.health == this.healthTotal;
-        /// <returns> Ratio of <see cref="health"/> relative to <see cref="healthTotal"/> (0~1) </returns>
-        internal float health_ratio
-            => this.health / this.healthTotal;
-        /// <returns> Precentage of <see cref="health"/> relative to <see cref="healthTotal"/> (0~100) </returns>
-        internal float health_percent
-            => this.health_ratio * 100;
+        internal bool IsImmortal => Defence == 1;
+        /// <returns> <see cref="Amount"/> &lt; <see cref="Total"/> </returns>
+        internal bool IsDamaged => Amount < Total;
+        /// <returns> <see cref="Amount"/> == <see cref="Total"/> </returns>
+        internal bool IsHealed => Amount.Value == Total;
+        /// <returns> Ratio of <see cref="Amount"/> relative to <see cref="Total"/> (0~1) </returns>
+        internal float Health_Ratio => Convert.ToSingle(Amount.Value) / Total;
+        /// <returns> Precentage of <see cref="Amount"/> relative to <see cref="Total"/> (0~100) </returns>
+        internal float Health_Percent => Health_Ratio * 100;
 
+        #endregion
+        #region Methods
+        private Amount _Health_Delta(float delta) {
+            if (IsImmortal || delta == 0) return Amount;
 
-        private float health_delta(float delta) {
-            if (delta == 0) return this.health;
-
-            float health_new = this.health + delta;
+            double health_new = Amount + delta;
 
             //! Never let health be negative or above total
             // to avoid any unexpected behavior
-            if (health_new > this.healthTotal)
-                this.restore_health();
-            else if (health_new <= 0)
-                this.kill();
+            if (health_new > Total) Restore();
+            else if (health_new <= 0) Kill();
 
-            this.health += delta;
-            return this.health;
+            Amount.Value += delta;
+            return Amount;
         }
 
-        /// <summary> Increase <see cref="health"/> directly using <paramref name="amount"/> </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float heal(float amount)
-            => this.health_delta(Math.Abs(amount));
 
-        /// <summary> Decrease <see cref="health"/> directly using <paramref name="amount"/> </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float hurt(float amount)
-            => this.health_delta(-Math.Abs(amount));
+        /// <summary> Increase <see cref="Amount"/> directly using the <paramref name="amount"/> specified </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal Amount Heal(float amount)
+            => _Health_Delta(amount.Positive());
 
-        /// <summary> Decrease <see cref="health"/> using <see cref="Damage"/> </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float hurtBy(Weapon weapon)
-            => this.hurt(weapon.damage.amount);
-
-        /// <summary> Decrease <see cref="health"/> using <see cref="Entity"/>.damage (if present) </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float hurtBy(Entity entity) {
-            bool hasWeapon = entity.stats.TryGetValue(StatName.Weapon, out Stat weapon);
-            if (hasWeapon)
-                this.hurtBy((Weapon)weapon);
-            return this.health;
+        /// <summary> Decrease <see cref="Amount"/> directly using the <paramref name="amount"/> specified </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal Amount Hurt(float amount, float piercing = 0) {
+            //? piercing 0—1
+            float piercing01 = piercing.AtOrBetween(0, 1);
+            //? pierced defence
+            float piercedDefence = (Defence - piercing01).Positive();
+            //? Calculate how much of the hurting amount has been defended
+            float amountDefended = amount * piercedDefence;
+            //? Reduce the original hurting amount using the amount that has been defended
+            float amountFinal = amount - amountDefended;
+            return _Health_Delta(amountFinal);
         }
 
-        /// <summary> Set <see cref="health"/> to the the maximum <see cref="healthTotal"/> </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float restore_health()
-            => this.health = this.healthTotal;
+        /// <summary> Decrease <see cref="Amount"/> using the <see cref="Damage"/> specified </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal Amount HurtBy(Damage damage)
+            => Hurt(damage.Amount, damage.Piercing);
+        /// <summary> Decrease <see cref="Amount"/> using the <see cref="Weapon"/> specified </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal Amount HurtBy(Weapon weapon)
+            => HurtBy(weapon.Damage);
 
-        /// <summary> Set <see cref="health"/> to 0 </summary>
-        /// <returns> Final <see cref="health"/> </returns>
-        internal float kill()
-            => this.health = 0;
+        /// <summary> Decrease <see cref="Amount"/> using <see cref="Entity"/>.damage (if present) </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal Amount HurtBy(Entity entity) {
+            var weapon = entity.Stats.Get<Weapon>();
+            if (weapon != null) HurtBy(weapon);
+            return Amount;
+        }
 
-        internal override StatName statName { get; }
-        /// <summary> Maximum value for <see cref="health"/> </summary>
+        /// <summary> Set <see cref="Amount"/> to the the maximum <see cref="Total"/> </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal float Restore()
+            => Convert.ToSingle(Amount.Value = Total);
+
+        /// <summary> Set <see cref="Amount"/> to 0 </summary>
+        /// <returns> Final <see cref="Amount"/> </returns>
+        internal float Kill()
+            => Convert.ToSingle(Amount.Value = 0);
+
+        internal void MakeImmortal()
+            => Defence = 1;
+        internal void MakeImmortal_Period(int timeMS) {
+            float currentDefence = Defence;
+            _ = new Timer(
+                (object originalDefence)
+                    => Defence = Convert.ToSingle(originalDefence),
+                currentDefence,
+                Timeout.Infinite,
+                timeMS);
+        }
+
+        #endregion
+
+
+        private Amount _Amount;
+        internal Amount Amount {
+            get => _Amount;
+            set {
+                if (value.Range.Min < 0)
+                    _Amount = new(value.Value, new(0, value.Range.Max));
+                else _Amount = value;
+            }
+        }
+
+        private float _Defence;
+        internal float Defence {
             get => _Defence;
-        internal float health { get; set; }
-
-        internal Health(float healthTotal) {
-            this.statName = StatName.Health;
-            this.healthTotal = healthTotal;
-            this.health = this.healthTotal;
+            set => _Defence = value.AtOrBetween(0, 1);
         }
+
+        internal Health(Amount amount, float defence = 0) {
+            Amount = amount;
+            Defence = defence;
+        }
+
+        internal Health Immortal()
+            => new(new Amount(1), defence: 1);
     }
 }
