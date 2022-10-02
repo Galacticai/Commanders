c22bb031bf87e18a4d9cbb4fa47cd36a2fb1a94a
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:48:24 +0300

    feat(redo): Vision `Stat` for `Entity`
    How an `Entity` sees...

--- Assets/Scripts/Game/Entities/Stats/Vision.cs
+++ Assets/Scripts/Game/Entities/Stats/Vision.cs
@@ -1,34 +1,11 @@
-using static Assets.Scripts.Game.Entities.Entity;
-
 namespace Assets.Scripts.Game.Entities.Stats {
+    /// <summary> How an <see cref="Entity"/> sees... </summary>
     internal sealed class Vision : Stat {
-        internal struct Default {
-            internal record TerritoryExtra {
-                internal const float Land = 0.0f;
-                internal const float Air = 1.5f;
-                internal const float Water = -1.0f;
-            }
-        }
-
-
-        #region Helper
-
-        internal float radius_byTerritory(Territory territory)
-            => this.radius_base
-            + territory switch {
-                Territory.Water => Default.TerritoryExtra.Water,
-                Territory.Air => Default.TerritoryExtra.Air,
-                //? Entity.Territory.Land
-                _ => Default.TerritoryExtra.Land
-            };
-
-        #endregion
-
-        internal override StatName statName { get; }
-        internal float radius_base { get; set; }
-        internal Vision(float radius) {
-            this.statName = StatName.Vision;
-            this.radius_base = radius;
+        internal float Radius { get; set; }
+        internal bool CamoVisible { get; set; }
+        internal Vision(float radius, bool camoVisible = false) {
+            Radius = radius;
+            CamoVisible = camoVisible;
         }
     }
 }
