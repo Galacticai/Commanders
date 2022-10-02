421a008c018f0ec0173d376d2f71c11af07adb08
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:41:17 +0300

    feat(unfinished): Control `Debuff` effect for `Entity`
    Let another `Commander` take over the `Entity`

---
+++ Assets/Scripts/Game/Entities/Stats/Effects/Debuffs/Control.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Game.Commanders;
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Game.Entities.Stats.Effects.Debuffs {
+    /// <summary> Let another <see cref="Commander"/> take over the <see cref="Entity"/> </summary>
+    internal class Control : Debuff {
+
+        internal Dictionary<Type, object> Modified => default;
+
+        internal override dynamic Get<IStat>(IStat stat) => default;
+
+        internal Commander Commander { get; set; }
+        internal Control(Commander commander, bool active = true)
+                : base(isUnique: true, active) {
+            Commander = commander;
+        }
+    }
+}
