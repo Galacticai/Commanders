8624aa6fbf653c3caac1190b6dfdf9458ae223f1
xeroling <nhkomaiha@hotmail.com>
2022-10-02 03:35:54 +0300

    feat: Debuff type of `Effect`

---
+++ Assets/Scripts/Game/Entities/Stats/Effects/Debuffs/Debuff.cs
@@ -0,0 +1,6 @@
+﻿namespace Assets.Scripts.Game.Entities.Stats.Effects.Debuffs {
+    internal abstract class Debuff : Effect {
+        public Debuff(bool isUnique, bool active)
+                    : base(EffectType.Debuff, isUnique, active) { }
+    }
+}
