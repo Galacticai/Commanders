20d0d28440d9ee652ebde52277bf436016860871
xeroling <nhkomaiha@hotmail.com>
2022-10-03 11:44:58 +0300

    feat(class): {Amount}: `double` value that exists in a `Range`

---
+++ Assets/Scripts/Lib/Math/Numerics/Amount.cs
@@ -0,0 +1,42 @@
+﻿/// —————————————————————————————————————————————
+//? 
+//!? 📜 Amount.cs
+//!? 🖋️ XEROling 📅 2022
+//!  ⚖️ GPL-3.0-or-later
+//?  🔗 Dependencies:
+//      + (XEROling) Math.Range.cs
+//      + (XEROling) Math.Range_Extension.cs
+//? 
+/// —————————————————————————————————————————————
+
+
+namespace Commanders.Assets.Scripts.Lib.Math.Numerics {
+    /// <summary> <see cref="double"/> value that exists in a <see cref="Numerics.Range"/> </summary>
+    public class Amount {
+        /// <summary> The ratio of <see cref="Value"/>,  relative to the <see cref="Range"/>'s min and max </summary>
+        public double Ratio_InRange
+            => Range.GetRatio(Value);
+
+        private double _Value;
+        public double Value {
+            get => _Value;
+            set => _Value = value.AtOrBetween(Range);
+        }
+        public Range Range { get; set; }
+
+        public Amount(double value) {
+            Value = value;
+            Range = new(0, value);
+        }
+        public Amount(double value, Range range) {
+            Value = value;
+            Range = range;
+        }
+
+        public static implicit operator double(Amount amount)
+            => amount.Value;
+        //!? Replacing the instance causes the Range to be reset which means old range data gets lost unexpectedly!
+        //!?    => Better create a new instance manually while knowing that a new range is made
+        //public static implicit operator Amount(double value) => new(value); 
+    }
+}
