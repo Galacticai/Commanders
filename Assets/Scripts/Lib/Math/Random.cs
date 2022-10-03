baea3a8215e7f7556984898e2d7b9814c2f1079e
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-03 23:00:11 +0300

    migrate {Common} contents to their corresponding namespaces

---
+++ Assets/Scripts/Lib/Math/Random.cs
@@ -0,0 +1,26 @@
+﻿/// —————————————————————————————————————————————
+//? 
+//!? 📜 Random.cs
+//!? 🖋️ Galacticai 📅 2022
+//!  ⚖️ GPL-3.0-or-later
+//?  🔗 Dependencies: No special dependencies
+//? 
+/// —————————————————————————————————————————————
+
+namespace Commanders.Assets.Scripts.Lib.Math {
+    public static class Random {
+        /// <returns> Random <see cref="int"/> such as:
+        /// <br/> <paramref name="max"/> &lt;= <see cref="int"/> &lt;= <paramref name="max"/></returns>
+        public static int GetRandom(int min, int max, System.Random random)
+            => random.Next(min, max);
+        /// <returns> Random <see cref="int"/> such as:
+        /// <br/> <paramref name="max"/> &lt;= <see cref="int"/> &lt;= <paramref name="max"/></returns>
+        public static int GetRandom(int min, int max)
+            => new System.Random().Next(min, max);
+
+        /// <summary> Chooses a random object from input array </summary> 
+        /// <returns>Random object from input array</returns>
+        public static T FromArray<T>(T[] array)
+            => array[GetRandom(0, array.Length - 1)]; //? -1 because it starts from 0
+    }
+}
