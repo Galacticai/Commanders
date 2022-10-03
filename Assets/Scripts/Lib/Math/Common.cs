9a1f57598cbeca748530d47763d1b465be2c0288
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-03 15:08:06 +0300

    migrate to `Math.Numerics.Range` and `Math.Arrays`

--- Assets/Scripts/Lib/Math/Common.cs
+++ Assets/Scripts/Lib/Math/Common.cs
@@ -35,53 +35,5 @@ namespace Commanders.Assets.Scripts.Lib.Math {
             public static type FromArray<type>(type[] arr)
                     => arr[Random.Int(0, arr.Length - 1)]; // -1 because it starts from 0
         }
-
-        /// <summary> Force input variable into the range of min and max </summary>
-        /// <param name="input">Value to process</param>
-        /// <param name="min">Minimum floor</param>
-        /// <param name="max">Maximum ceiling</param>
-        /// <returns><list type="bullet">
-        /// <item>Returns <paramref name="max"/> — if <paramref name="input"/> &gt; <paramref name="max"/></item>
-        /// <item>Returns <paramref name="min"/> — if <paramref name="input"/> &lt; <paramref name="min"/></item>
-        /// <item>Returns <paramref name="input"/> — if already in range</item>
-        /// </list></returns>
-        public static double ForcedInRange(double input, double min, double max) {
-            if (input > max) return max;
-            if (input < min) return min;
-            return input;
-        }
-
-        /// <summary> Check if <paramref name="input"/> is in range </summary>
-        /// <param name="input">Value to process</param>
-        /// <param name="min">Minimum floor</param>
-        /// <param name="max">Maximum ceiling</param>
-        /// <returns>true if input is in [<paramref name="min"/>, <paramref name="max"/>] range. Else false</returns>
-        public static bool IsInRange(double input, double min, double max)
-            => input >= min & input <= max;
-
-        public struct Arrays {
-
-            /// <summary> Add an <paramref name="element"/> item to the end of <paramref name="array"/></summary>
-            /// <typeparam name="type">Type of the array to be used</typeparam>
-            /// <param name="array">Array to manipulate</param>
-            /// <param name="element">Element to add to <paramref name="array"/></param>
-            /// <returns>Array {<paramref name="array"/>, <paramref name="element"/>} as <typeparamref name="type"/>[]</returns>
-            public static type[] AddArrays<type>(type[] array, type element)
-                => AddArrays(array, new type[] { element });
-            /// <summary> Add an <paramref name="expansion"/> array to the end of <paramref name="array"/></summary>
-            /// <typeparam name="type">Type of the array to be used</typeparam>
-            /// <param name="array">Array to manipulate</param>
-            /// <param name="expansion">Element to add to <paramref name="array"/></param>
-            /// <returns>Array {<paramref name="array"/>, <paramref name="expansion"/>} as <typeparamref name="type"/>[]</returns>
-            public static type[] AddArrays<type>(type[] array, type[] expansion)
-                => array.Concat(expansion).ToArray();
-            //{
-            //type[] expandedArray = new type[array.Length + 1]; // set expandedArray size to "length" (bigger by 1)
-            //for (int i = 0; i < array.Length; i++)
-            //    expandedArray[i] = array[i]; // copy all elements from array to expandedArray
-            //expandedArray[array.Length] = element; // add the final element
-            //return expandedArray;
-            //}
-        }
     }
 }
