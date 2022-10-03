889549f6bca87dd208046ec07c09cd5d3e46e17e
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-03 12:10:37 +0300

    feat(class disabled unfinished): {Number}: a number that can be of any numeric type
    ! Disabled ! bad performance, not finished

---
+++ Assets/Scripts/Lib/Math/Numerics/Number.cs
@@ -0,0 +1,64 @@
+﻿//using System;
+
+namespace Commanders.Assets.Scripts.Lib.Math.Numerics
+{
+//namespace Assets.Scripts.Lib.Math.Numerics;
+//public class Number<TNumber>
+//                where TNumber : IComparable, IFormattable, IConvertible, IComparable<TNumber>, IEquatable<TNumber> {
+
+//    #region Helper
+
+//    public double ToDouble()
+//        => Convert.ToDouble(Value);
+//    public double ToInt32()
+//        => Convert.ToInt32(Value);
+//    public double ToLong()
+//        => Convert.ToSingle(Value);
+
+//    #endregion
+
+
+//    public TNumber Value { get; set; }
+
+//    public Number(TNumber value) {
+//        Value = value;
+//    }
+
+
+//    #region Operators
+
+//    public static Number<TNumber> operator +(Number<TNumber> number1, Number<TNumber> number2)
+//        => new((dynamic)number1.Value + (dynamic)number2.Value);
+//    public static Number<TNumber> operator -(Number<TNumber> number1, Number<TNumber> number2)
+//        => new((dynamic)number1.Value - (dynamic)number2.Value);
+//    public static Number<TNumber> operator /(Number<TNumber> number1, Number<TNumber> number2)
+//        => new((dynamic)number1.Value / (dynamic)number2.Value);
+//    public static Number<TNumber> operator *(Number<TNumber> number1, Number<TNumber> number2)
+//        => new((dynamic)number1.Value * (dynamic)number2.Value);
+
+//    public override bool Equals(object obj) {
+//        if (obj == null) return false;
+//        try {
+//            var number = obj as Number<TNumber>;
+//            if (number == null) return false;
+//            return this == number;
+//        } catch { return false; }
+//    }
+//    public override int GetHashCode() => HashCode.Combine(Value);
+//    public static bool operator ==(Number<TNumber> number1, Number<TNumber> number2)
+//        => (dynamic)number1.Value == (dynamic)number2.Value;
+//    public static bool operator !=(Number<TNumber> number1, Number<TNumber> number2)
+//        => (dynamic)number1.Value != (dynamic)number2.Value;
+//    public static bool operator >(Number<TNumber> number1, Number<TNumber> number2)
+//        => (dynamic)number1.Value > (dynamic)number2.Value;
+//    public static bool operator <(Number<TNumber> number1, Number<TNumber> number2)
+//        => (dynamic)number1.Value < (dynamic)number2.Value;
+
+//    public static implicit operator TNumber(Number<TNumber> number)
+//        => number.Value;
+//    public static implicit operator Number<TNumber>(TNumber number)
+//        => new(number);
+
+//    #endregion
+//}
+}
