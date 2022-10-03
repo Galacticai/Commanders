7ea2dfa25803cfbed4827d2bcbd695d02e35713f
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-03 15:55:11 +0300

    feat(class): {Objects}: various tools for `object`s

---
+++ Assets/Scripts/Lib/Objects.cs
@@ -0,0 +1,35 @@
+﻿/// —————————————————————————————————————————————
+//? 
+//!? 📜 Objects.cs
+//!? 🖋️ Galacticai 📅 2022
+//!  ⚖️ GPL-3.0-or-later
+//?  🔗 Dependencies: No special dependencies
+//? 
+/// —————————————————————————————————————————————
+
+
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace Commanders.Assets.Scripts.Lib {
+    /// <summary> Various tools for <see cref="object"/>s </summary>
+    public class Objects {
+        public static IEnumerable<Type> FindSubClassesOf<T>() {
+            var baseType = typeof(T);
+            return baseType
+                .Assembly.GetTypes()
+                .Where(t => t.IsSubclassOf(baseType));
+        }
+
+        public static ExpandoObject Combine_ExpandoObjects(ExpandoObject obj1, ExpandoObject obj2) {
+            IDictionary<string, object> dict1 = obj1;
+            IDictionary<string, object> dict2 = obj2;
+            IDictionary<string, object> result = new ExpandoObject();
+            foreach (var (Key, Value) in dict1.Concat(dict2))
+                result[Key] = Value;
+            return (ExpandoObject)result;
+        }
+    }
+}
