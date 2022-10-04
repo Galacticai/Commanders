dd2daa0a665ada46fe2513bc8f44d2ca89bd05c1
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-04 16:13:54 +0300

    update(moved): use the new `Space3D.PlaneType` enum

--- Assets/Scripts/Lib/Unity/FollowPointer.cs
+++ Assets/Scripts/Lib/Unity/FollowPointer.cs
@@ -1,9 +1,9 @@
-using Commanders.Assets.Scripts.Lib.Math;
+using Commanders.Assets.Scripts.Lib.Math.Space3D;
 using UnityEngine;
 
 namespace Commanders.Assets.Scripts.Lib.Unity {
     public class FollowPointer : MonoBehaviour {
-        [SerializeField] private Common.Space.Plane _TargetPlane;
+        [SerializeField] private PlaneType _TargetPlane;
         [SerializeField] private Canvas _AlignToCanvas;
         private Transform _Transform;
 
@@ -13,9 +13,9 @@ namespace Commanders.Assets.Scripts.Lib.Unity {
         private void Update() {
             if (_Transform == null) return;
             Vector3 newPos = _TargetPlane switch {
-                Common.Space.Plane.yz => new(_Transform.position.x, Input.mousePosition.x, Input.mousePosition.y),
-                Common.Space.Plane.xz => new(Input.mousePosition.x, _Transform.position.y, Input.mousePosition.y),
-                //Common.Space.Plane.xy 
+                PlaneType.YZ => new(_Transform.position.x, Input.mousePosition.x, Input.mousePosition.y),
+                PlaneType.XZ => new(Input.mousePosition.x, _Transform.position.y, Input.mousePosition.y),
+                //!? PlaneType.XY 
                 _ => new(Input.mousePosition.x, Input.mousePosition.y, _Transform.position.z),
             };
             if (_AlignToCanvas != null) {
