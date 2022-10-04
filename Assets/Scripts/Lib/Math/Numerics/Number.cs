4b7422bf38722d26bb0baee21611171a29bd6d08
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-04 15:25:44 +0300

    feat(class): {Number}: just a number...

--- Assets/Scripts/Lib/Math/Numerics/Number.cs
+++ Assets/Scripts/Lib/Math/Numerics/Number.cs
@@ -1,64 +1,64 @@
-﻿//using System;
+﻿using System;
 
-namespace Commanders.Assets.Scripts.Lib.Math.Numerics
-{
-//namespace Assets.Scripts.Lib.Math.Numerics;
-//public class Number<TNumber>
-//                where TNumber : IComparable, IFormattable, IConvertible, IComparable<TNumber>, IEquatable<TNumber> {
+namespace Commanders.Assets.Scripts.Lib.Math.Numerics {
+    /// <summary> Just a number </summary>
+    /// <typeparam name="TNumber"> Number value type </typeparam>
+    public class Number<TNumber>
+                    where TNumber : IComparable, IFormattable, IConvertible, IComparable<TNumber>, IEquatable<TNumber> {
 
-//    #region Helper
+        #region Helper
 
-//    public double ToDouble()
-//        => Convert.ToDouble(Value);
-//    public double ToInt32()
-//        => Convert.ToInt32(Value);
-//    public double ToLong()
-//        => Convert.ToSingle(Value);
+        public double ToDouble()
+            => Convert.ToDouble(Value);
+        public double ToInt32()
+            => Convert.ToInt32(Value);
+        public double ToLong()
+            => Convert.ToSingle(Value);
 
-//    #endregion
-
-
-//    public TNumber Value { get; set; }
-
-//    public Number(TNumber value) {
-//        Value = value;
-//    }
+        #endregion
 
 
-//    #region Operators
+        public TNumber Value { get; set; }
 
-//    public static Number<TNumber> operator +(Number<TNumber> number1, Number<TNumber> number2)
-//        => new((dynamic)number1.Value + (dynamic)number2.Value);
-//    public static Number<TNumber> operator -(Number<TNumber> number1, Number<TNumber> number2)
-//        => new((dynamic)number1.Value - (dynamic)number2.Value);
-//    public static Number<TNumber> operator /(Number<TNumber> number1, Number<TNumber> number2)
-//        => new((dynamic)number1.Value / (dynamic)number2.Value);
-//    public static Number<TNumber> operator *(Number<TNumber> number1, Number<TNumber> number2)
-//        => new((dynamic)number1.Value * (dynamic)number2.Value);
+        public Number(TNumber value) {
+            Value = value;
+        }
 
-//    public override bool Equals(object obj) {
-//        if (obj == null) return false;
-//        try {
-//            var number = obj as Number<TNumber>;
-//            if (number == null) return false;
-//            return this == number;
-//        } catch { return false; }
-//    }
-//    public override int GetHashCode() => HashCode.Combine(Value);
-//    public static bool operator ==(Number<TNumber> number1, Number<TNumber> number2)
-//        => (dynamic)number1.Value == (dynamic)number2.Value;
-//    public static bool operator !=(Number<TNumber> number1, Number<TNumber> number2)
-//        => (dynamic)number1.Value != (dynamic)number2.Value;
-//    public static bool operator >(Number<TNumber> number1, Number<TNumber> number2)
-//        => (dynamic)number1.Value > (dynamic)number2.Value;
-//    public static bool operator <(Number<TNumber> number1, Number<TNumber> number2)
-//        => (dynamic)number1.Value < (dynamic)number2.Value;
 
-//    public static implicit operator TNumber(Number<TNumber> number)
-//        => number.Value;
-//    public static implicit operator Number<TNumber>(TNumber number)
-//        => new(number);
+        #region Operators
 
-//    #endregion
-//}
+        public static Number<TNumber> operator +(Number<TNumber> number1, Number<TNumber> number2)
+            => new((dynamic)number1.Value + (dynamic)number2.Value);
+        public static Number<TNumber> operator -(Number<TNumber> number1, Number<TNumber> number2)
+            => new((dynamic)number1.Value - (dynamic)number2.Value);
+        public static Number<TNumber> operator /(Number<TNumber> number1, Number<TNumber> number2)
+            => new((dynamic)number1.Value / (dynamic)number2.Value);
+        public static Number<TNumber> operator *(Number<TNumber> number1, Number<TNumber> number2)
+            => new((dynamic)number1.Value * (dynamic)number2.Value);
+
+        public override bool Equals(object obj) {
+            if (obj == null) return false;
+            try {
+                var number = obj as Number<TNumber>;
+                if (number == null) return false;
+                return this == number;
+            } catch { return false; }
+        }
+        public override int GetHashCode() => HashCode.Combine(Value);
+        public static bool operator ==(Number<TNumber> number1, Number<TNumber> number2)
+            => (dynamic)number1.Value == (dynamic)number2.Value;
+        public static bool operator !=(Number<TNumber> number1, Number<TNumber> number2)
+            => (dynamic)number1.Value != (dynamic)number2.Value;
+        public static bool operator >(Number<TNumber> number1, Number<TNumber> number2)
+            => (dynamic)number1.Value > (dynamic)number2.Value;
+        public static bool operator <(Number<TNumber> number1, Number<TNumber> number2)
+            => (dynamic)number1.Value < (dynamic)number2.Value;
+
+        public static implicit operator TNumber(Number<TNumber> number)
+            => number.Value;
+        public static implicit operator Number<TNumber>(TNumber number)
+            => new(number);
+
+        #endregion
+    }
 }
