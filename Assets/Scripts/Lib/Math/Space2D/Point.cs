13f3edb7ef2d77ba3be90823196f4875023f6ad6
Galacticai <24789792+Galacticai@users.noreply.github.com>
2022-10-11 22:49:38 +0300

    fix: typos

--- Assets/Scripts/Lib/Math/Space2D/Point.cs
+++ Assets/Scripts/Lib/Math/Space2D/Point.cs
@@ -16,8 +16,8 @@ namespace Commanders.Assets.Scripts.Lib.Math.Space2D {
 
         public double Distance(Point point)
             => sMath.Sqrt(
-                sMath.Pow(x - point.x, 2)
-                + sMath.Pow(y - point.y, 2));
+                sMath.Pow(X - point.X, 2)
+                + sMath.Pow(Y - point.Y, 2));
 
         public double DistanceToOrigin
             => Distance(ORIGIN);
